0c9ae773b3bc1c91a261c1ee3625bdcc234341cd
Haroldas <hpharis258@gmail.com>
2024-05-02 22:00:08 +0100

    basic login

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,62 +5,139 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import React, { useState } from 'react';
+import { supabase } from '../database/database';
+import { useEffect } from 'react';
 
 function AppNavbar() {
-  return (
-    <>
+    const [session, setSession] = useState(null);
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+          setSession(session)
+        })
+        const {
+          data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+          setSession(session)
+        })
+        return () => subscription.unsubscribe()
+      }, [])
+    if(!session){
+        return (
+            <>
+              
+                <Navbar key={1} expand={'md'} className="bg-body-tertiary mb-3">
+                  <Container fluid>
+                    <Navbar.Brand href="/">Blogsomething.com</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'md'}`} />
+                    <Navbar.Offcanvas
+                      id={`offcanvasNavbar-expand-${'md'}`}
+                      aria-labelledby={`offcanvasNavbarLabel-expand-${'md'}`}
+                      placement="end"
+                    >
+                      <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'md'}`}>
+                          Offcanvas
+                        </Offcanvas.Title>
+                      </Offcanvas.Header>
+                      <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                          <Nav.Link href="/">Home</Nav.Link>
+                          <Nav.Link href="/about">About</Nav.Link>
+                          <NavDropdown
+                            title="More"
+                            id={`offcanvasNavbarDropdown-expand-${'md'}`}
+                          >
+                            <NavDropdown.Item href="/create">Create a Blog</NavDropdown.Item>
+                            <NavDropdown.Item href="/auth">
+                              Login or Register
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/support">
+                              Support
+                            </NavDropdown.Item>
+                           
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/donate">
+                              Buy me a coffee
+                            </NavDropdown.Item>
+                          </NavDropdown>
+                        </Nav>
+                        <Form className="d-flex">
+                          <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                          />
+                          <Button variant="outline-success">Search</Button>
+                        </Form>
+                      </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                  </Container>
+                </Navbar>
+        
+            </>
+          );
+    } else {
+        return (
+            <>
+              
+                <Navbar key={1} expand={'md'} className="bg-body-tertiary mb-3">
+                  <Container fluid>
+                    <Navbar.Brand href="/">Blogsomething.com</Navbar.Brand>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'md'}`} />
+                    <Navbar.Offcanvas
+                      id={`offcanvasNavbar-expand-${'md'}`}
+                      aria-labelledby={`offcanvasNavbarLabel-expand-${'md'}`}
+                      placement="end"
+                    >
+                      <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'md'}`}>
+                          Offcanvas
+                        </Offcanvas.Title>
+                      </Offcanvas.Header>
+                      <Offcanvas.Body>
+                        <Nav className="justify-content-end flex-grow-1 pe-3">
+                          <Nav.Link href="/">Home</Nav.Link>
+                          <Nav.Link href="/about">About</Nav.Link>
+                          <NavDropdown
+                            title="More"
+                            id={`offcanvasNavbarDropdown-expand-${'md'}`}
+                          >
+                            <NavDropdown.Item href="/create">Create a Blog</NavDropdown.Item>
+                            <NavDropdown.Item href="/auth">
+                              Login or Register
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="/support">
+                              Support
+                            </NavDropdown.Item>
+                            <NavDropdown.Item onClick={async () => { await supabase.auth.signOut()}}>
+                              Log out
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/donate">
+                              Buy me a coffee
+                            </NavDropdown.Item>
+                          </NavDropdown>
+                        </Nav>
+                        <Form className="d-flex">
+                          <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                          />
+                          <Button variant="outline-success">Search</Button>
+                        </Form>
+                      </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                  </Container>
+                </Navbar>
+        
+            </>
+          );
+    }
       
-        <Navbar key={1} expand={'md'} className="bg-body-tertiary mb-3">
-          <Container fluid>
-            <Navbar.Brand href="/">Blogsomething.com</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${'md'}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${'md'}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${'md'}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${'md'}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/about">About</Nav.Link>
-                  <NavDropdown
-                    title="More"
-                    id={`offcanvasNavbarDropdown-expand-${'md'}`}
-                  >
-                    <NavDropdown.Item href="/create">Create a Blog</NavDropdown.Item>
-                    <NavDropdown.Item href="/auth">
-                      Login or Register
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="/support">
-                      Support
-                    </NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="/donate">
-                      Buy me a coffee
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-
-    </>
-  );
 }
 
 export default AppNavbar;
--- src/database/database.js
+++ src/database/database.js
@@ -2,15 +2,51 @@ import { createClient } from '@supabase/supabase-js';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { redirect } from "react-router-dom";
+
 
 console.log(process.env.REACT_APP_SUPABASE_URL);
 
 const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_API_KEY);
 
 function AppAuth() {
-  return (
-    <Auth supabaseClient={supabase} />
-  );
+     // Supabase Session
+  const [session, setSession] = useState(null);
+  const [Email, setEmail] = React.useState("");
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    return () => subscription.unsubscribe()
+  }, [])
+  
+    if(!session) {
+      return <Auth supabaseClient={supabase} providers={Email} appearance={{ theme: ThemeSupa }} />
+    }else{
+        return(
+            <>
+            <div className='container'>
+                <h1>Welcome {session.user.email}</h1>
+                <h2>You have logged in successfully!</h2>
+
+                <button onClick={async () => {
+                  await supabase.auth.signOut()
+                }}>Sign Out</button>
+            </div>
+            </>
+        );
+        
+    
+    }
+  
 }
 
 export default AppAuth;
+export {supabase};
